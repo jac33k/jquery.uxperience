bfea686c0e7b08f8dc1b60fb53fda79bd3546c22
Jacek Brożek <jac33k@gmail.com>
2014-02-14 16:51:19 +0100

    wrap jquery element in custom Field object

--- jquery.uxperience.js
+++ jquery.uxperience.js
@@ -1,6 +1,33 @@
 
 (function ($) {
-  var defaultOptions, UXperience, fields = [];
+  var defaultOptions, UXperience, Field, fields = [];
+
+  __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+
+  Field = (function() {
+    function Field(el) {
+      this.focusCallback = __bind(this.focusCallback, this);
+      this.blurCallback = __bind(this.blurCallback, this);
+
+      this.time = 0;
+      this.el = $(el);
+      this.name = this.el.attr('name');
+
+      this.el.bind('focus', this.focusCallback);
+      this.el.bind('blur', this.blurCallback);
+    };
+
+    Field.prototype.focusCallback = function() {
+      return this.start_time = Date.now();
+    };
+
+    Field.prototype.blurCallback = function() {
+      var diff = Date.now() - this.start_time;
+      return this.time += diff;
+    };
+
+    return Field;
+  })();
 
   UXperience = (function(){
     function UXperience(handler, options) {
@@ -11,7 +38,7 @@
       elements = handler.children('input:not([type=submit], [type=radio]), select, textarea'); // TODO: whitelist option maybe
 
       elements.each(function(i, el) {
-        fields.push(el);
+        fields.push(new Field(el));
       });
 
       results = {}
@@ -19,7 +46,7 @@
       _this = this;
       handler.bind('submit', function() {
         $.each(fields, function(i, el) {
-          return results[el.data('name')] = el.data('time');
+          return results[el.name] = el.time;
         });
         if(typeof options.submit === 'function') {
           results.overall_time = Date.now() - _this.start_time;
